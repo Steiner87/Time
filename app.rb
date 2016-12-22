6fb3c6bc35f9e204bc70102ec693e4b549d96205
Igor Pugachov <i.pugachev@makewear.com.ua>
2016-12-22 14:09:11 +0200

    Add right andings.

--- app.rb
+++ app.rb
@@ -4,31 +4,40 @@ def time ts1,ts2
 	t=Time.at(x)
 
 
-	arr=t.day.to_i.to_s
+	d=t.day.to_i.to_s
+	h=t.hour.to_i.to_s
+	m=t.min.to_i.to_s
+	s=t.sec.to_i.to_s
 
-
-	if arr[-1]=='1' && arr[-2]!='1'
-		print "#{t.day} day "
-	else
-		print "#{t.day} days "
+	if 	d.to_i!=0
+		if d[-1]=='1' && d[-2]!='1'
+			print "#{t.day} day "
+		else
+			print "#{t.day} days "
+		end
+	end
+	if 	h.to_i!=0
+		if h[-1]=='1' && h[-2]!='1'
+			print "#{t.hour} hour "
+		else
+			print "#{t.hour} hours "
+		end
 	end
 	
-		if arr[-1]=='1' && arr[-2]!='1'
-		print "#{t.hour} hour "
-	else
-		print "#{t.hour} hours "
+	if 	m.to_i!=0
+		if m[-1]=='1' && m[-2]!='1'
+			print "#{t.min} min "
+		else
+			print "#{t.min} minuts "
+		end
 	end
 	
-		if arr[-1]=='1' && arr[-2]!='1'
-		print "#{t.min} min "
-	else
-		print "#{t.min} minuts "
-	end
-	
-	if arr[-1]=='1' && arr[-2]!='1'
-		print "#{t.sec} sec "
-	else
-		print "#{t.sec} seconds "
+	if 	s.to_i!=0
+		if s[-1]=='1' && s[-2]!='1'
+			print "and #{t.sec} sec "
+		else
+			print "and #{t.sec} seconds "
+		end
 	end
 
 
