717e62d677b8dd5602ea5b776c1be182fdea807d
Igor Pugachov <i.pugachev@makewear.com.ua>
2016-12-25 16:06:25 +0200

    Create class A in app.rb

--- app.rb
+++ app.rb
@@ -1,47 +1,76 @@
 
-def time ts1,ts2
-	x=ts1-ts2
-	t=Time.at(x)
+ class A
+ 
+    def initialize x,y
+        @ts1=x
+		@ts2=y
+		time @ts1,@ts2
+    end
+
+	def time ts1,ts2
+		x=ts1-ts2 #вычисляем разницу двух дат в системе юникс
+		t=Time.at(x)# t-дата стандартного вида
 
 
-	d=t.day.to_i.to_s
-	h=t.hour.to_i.to_s
-	m=t.min.to_i.to_s
-	s=t.sec.to_i.to_s
+		d=t.day.to_i.to_s 
+		h=t.hour.to_i.to_s
+		m=t.min.to_i.to_s
+		s=t.sec.to_i.to_s
 
-	if 	d.to_i!=0
-		if d[-1]=='1' && d[-2]!='1'
-			print "#{t.day} day "
-		else
-			print "#{t.day} days "
+		#Определяем какие слова подставлять
+		if 	d.to_i!=0
+			if d[-1]=='1' && d[-2]!='1'
+				print "#{t.day} day "
+			else
+				print "#{t.day} days "
+			end
 		end
-	end
-	if 	h.to_i!=0
-		if h[-1]=='1' && h[-2]!='1'
-			print "#{t.hour} hour "
-		else
-			print "#{t.hour} hours "
+		
+		if 	h.to_i!=0
+			if h[-1]=='1' && h[-2]!='1'
+				print "#{t.hour} hour "
+			else
+				print "#{t.hour} hours "
+			end
 		end
-	end
-	
-	if 	m.to_i!=0
-		if m[-1]=='1' && m[-2]!='1'
-			print "#{t.min} min "
-		else
-			print "#{t.min} minuts "
+		
+		if 	m.to_i!=0
+			if m[-1]=='1' && m[-2]!='1'
+				print "#{t.min} min "
+			else
+				print "#{t.min} minuts "
+			end
 		end
-	end
-	
-	if 	s.to_i!=0
-		if s[-1]=='1' && s[-2]!='1'
-			print "and #{t.sec} sec "
-		else
-			print "and #{t.sec} seconds "
+		
+		if 	s.to_i!=0
+			if s[-1]=='1' && s[-2]!='1'
+				print "and #{t.sec} sec "
+			else
+				print "and #{t.sec} seconds "
+			end
 		end
+
+		return 1 
 	end
-
-
-
+ 
+ 
 end
-
- time rand(Time.now.to_i),2592000
+ 
+ obj=A.new rand(Time.now.to_i),2592000 # Создаем объект и вводим в него два значения в формате юникс.
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
--- app2.rb
+++ app2.rb
@@ -1,32 +1,19 @@
 
+
 def get_dates years,months,days,hours=0,minuts=0,seconds=0
     
-#   1) Создаем объект Time и присваиваем ему введенные значения
+#   1) Создаем объект date и присваиваем ему введенные значения
 	date=Time.new(years,months,days,hours,minuts,seconds) 
     
 #   2)Переводим объект в цифровое Unix отображение
 	date_unix=date.to_i #Дата в Unix системе исчесления
-    
-#    Функция переводящая юниксдату в стандартную
-    Time.at(date_unix)
-    
-    years=Time.at(date_unix).year
-    months=Time.at(date_unix).month
-    days=Time.at(date_unix).day
-    hours=Time.at(date_unix).hour
-    minuts=Time.at(date_unix).min
-    seconds=Time.at(date_unix).sec
-    
-    first_day=days
-    
-#    puts date=Time.new(years,months,days,hours,minuts,seconds)
-    puts date.strftime "%a, %d %b %Y %H:%M:%S"
+       
+    puts date.strftime "%a, %d %b %Y %H:%M:%S" #Выводим дату в заданом формате впервый раз
     date_unix=date.to_i
-#    puts Time.at(date_unix)
+    
+    first_day=days #Фиксируем день с которого был начан отсчет
     
 #   3) Создаем цикл для вывода даты с интервалом в 15 мин 
-    
-    h=date_unix+60*60
     
     x=true
     while x
@@ -36,20 +23,12 @@ def get_dates years,months,days,hours=0,minuts=0,seconds=0
         if hours1!=hours2
             x=false
         end
-#        puts Time.at(date_unix)
         
-        years=Time.at(date_unix).year
-        months=Time.at(date_unix).month
-        days=Time.at(date_unix).day
-        hours=Time.at(date_unix).hour
-        minuts=Time.at(date_unix).min
-        seconds=Time.at(date_unix).sec
-
-        date=Time.new(years,months,days,hours,minuts,seconds)
-        puts date.strftime "%a, %d %b %Y %H:%M:%S"
+        get_settings date_unix
     
     end
     puts '==============================================='
+#   4) Создаем цикл для вывода даты с интервалом в 30 мин 
     x=0
     while x!=5
         hours1=Time.at(date_unix).hour
@@ -59,52 +38,47 @@ def get_dates years,months,days,hours=0,minuts=0,seconds=0
             hours1=hours2
             x+=1
         end
-#        puts Time.at(date_unix)
-        
-        years=Time.at(date_unix).year
-        months=Time.at(date_unix).month
-        days=Time.at(date_unix).day
-        hours=Time.at(date_unix).hour
-        minuts=Time.at(date_unix).min
-        seconds=Time.at(date_unix).sec
 
-        date=Time.new(years,months,days,hours,minuts,seconds)
-        puts date.strftime "%a, %d %b %Y %H:%M:%S"
+        
+        get_settings date_unix
     
     end
     
      puts '==============================================='
-    
+#   5) Создаем цикл для вывода даты с интервалом в 1 час
         x=0
     while x!=7
+        
         date_unix+=60*60
 
-            x+=1
+        x+=1
 
-#        puts Time.at(date_unix)
-        
-        years=Time.at(date_unix).year
-        months=Time.at(date_unix).month
-        days=Time.at(date_unix).day
-        hours=Time.at(date_unix).hour
-        minuts=Time.at(date_unix).min
-        seconds=Time.at(date_unix).sec
-
-        date=Time.new(years,months,days,hours,minuts,seconds)
-        puts date.strftime "%a, %d %b %Y %H:%M:%S"
+        get_settings date_unix
     
     end
     
      puts '==============================================='
-    
+#   6) Создаем цикл для вывода даты с интервалом в 24 часа 
         x=first_day
     while x!=first_day+4
+        
         date_unix+=24*60*60
 
-            x+=1
-
-#        puts Time.at(date_unix)
+        x+=1
         
+        get_settings date_unix
+    
+    end
+    
+     puts '==============================================='
+    
+  return 1  
+end 
+
+
+
+def get_settings date_unix
+    
         years=Time.at(date_unix).year
         months=Time.at(date_unix).month
         days=Time.at(date_unix).day
@@ -114,11 +88,6 @@ def get_dates years,months,days,hours=0,minuts=0,seconds=0
 
         date=Time.new(years,months,days,hours,minuts,seconds)
         puts date.strftime "%a, %d %b %Y %H:%M:%S"
-    
-    end
-    
-     puts '==============================================='
-    
     
 end
 
